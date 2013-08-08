f83a25f11f3bb4cb2188bf1e9c3160d2e08f0cb2
Stephanie Leary <steph@sillybean.net>
2013-08-07 21:32:55 -0500

    Show action link only if child pages exist

--- hide-deep-pages.php
+++ hide-deep-pages.php
@@ -4,7 +4,7 @@ Plugin Name: Hide Deep Pages
 Plugin URI: http://stephanieleary.com
 Description: Plugin to hide child pages on Edit screens unless specifically requested.
 Author: Stephanie Leary
-Version: 0.1
+Version: 0.1.1
 Author URI: http://stephanieleary.com
 */
 
@@ -30,9 +30,12 @@ function hide_deep_pages_where( $where ) {
 add_filter( 'page_row_actions', 'hide_deep_pages_action_links', 10, 2 );
 
 function hide_deep_pages_action_links( $actions, $page ) {
-	
-    $action_url = add_query_arg( array( 'child_of' => $page->ID ) );
-    $actions['show_child_pages'] = sprintf( '<a href="%s">%s</a>' , $action_url, __( 'Show Child Pages' ) );
 
+    $child_pages = get_posts( array( 'post_status' => 'any', 'post_type' => 'page', 'post_parent' => $page->ID ) );
+    if ( count( $child_pages ) > 0 ) {
+        $action_url = add_query_arg( array( 'child_of' => $page->ID ) );
+        $actions['show_child_pages'] = sprintf( '<a href="%s">%s</a>' , $action_url, __( 'Show Child Pages' ) );
+    }
+    
     return $actions;
 }
