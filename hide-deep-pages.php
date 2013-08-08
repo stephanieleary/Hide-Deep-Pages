fcbe0c44a0c5123113015b378648bd69d5b2dd74
Stephanie Leary <steph@sillybean.net>
2013-08-07 21:23:33 -0500

    Initial commit

---
+++ hide-deep-pages.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: Hide Deep Pages
+Plugin URI: http://stephanieleary.com
+Description: Plugin to hide child pages on Edit screens unless specifically requested.
+Author: Stephanie Leary
+Version: 0.1
+Author URI: http://stephanieleary.com
+*/
+
+/*
+TODO:
+-- Ajaxify
+
+/**/
+
+add_filter( 'posts_where' , 'hide_deep_pages_where' );
+
+function hide_deep_pages_where( $where ) {
+
+    if ( is_admin() ) {
+        if ( isset( $_REQUEST['child_of'] ) && !empty( $_REQUEST['child_of'] ) && intval( $_REQUEST['child_of'] ) != 0 )
+            $where .= sprintf(" AND post_parent = %d", intval( $_REQUEST['child_of'] ) );
+        else
+            $where .= sprintf(" AND post_parent = %d", 0 );
+    }
+    return $where;
+}
+
+add_filter( 'page_row_actions', 'hide_deep_pages_action_links', 10, 2 );
+
+function hide_deep_pages_action_links( $actions, $page ) {
+	
+    $action_url = add_query_arg( array( 'child_of' => $page->ID ) );
+    $actions['show_child_pages'] = sprintf( '<a href="%s">%s</a>' , $action_url, __( 'Show Child Pages' ) );
+
+    return $actions;
+}
